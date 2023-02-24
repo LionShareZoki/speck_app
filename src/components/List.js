9ff55f1f64985eab7b5f2fd74dc6224d6980d0c1
Zoran <pujic.zoran@gmail.com>
2023-02-24 18:33:39 +0100

    create html

---
+++ src/components/List.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function TodoList() {
+  const [items, setItems] = useState([{}]);
+  return (
+    <div>
+      <h1>Todo List</h1>
+    </div>
+  );
+}
+
+export default TodoList;
