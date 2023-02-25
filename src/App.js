5f004022fbe4041c16c535f40a90b15e6ecc1abc
Zoran <pujic.zoran@gmail.com>
2023-02-25 15:19:00 +0100

    Project finished

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import List from "./components/List";
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>My technologies</h1>
+      <header className="heading">
+        <h1>MY TECHNOLOGIES</h1>
       </header>
       <div>
         <List />
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,46 +2,55 @@ import React, { useState } from "react";
 
 function List() {
   const [items, setItems] = useState([]);
-  function handleSubmit(e) {
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const newItem = { id: Date.now(), text: e.target[0].value };
     const inputValue = e.target[0].value.trim();
     if (inputValue !== "") {
-      setItems([newItem, ...items]);
+      const newItem = { id: Date.now(), text: inputValue };
+      setItems((prevItems) => [newItem, ...prevItems]);
       e.target.reset();
     }
-  }
-  function handleClear() {
+  };
+
+  const handleClear = () => {
     setItems([]);
-  }
+  };
+
   const handleRemoveItem = (id) => {
-    setItems(items.filter((item) => item.id !== id));
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  const renderItems = () => {
+    return items.map((item) => (
+      <li
+        key={item.id}
+        className="card fade-in"
+        onClick={() => handleRemoveItem(item.id)}
+      >
+        {item.text}
+      </li>
+    ));
   };
 
   return (
     <div>
       <div className="input_and_button">
-        <div>
-          <form onSubmit={handleSubmit}>
-            <input type="text" />
-            <button type="submit">Add</button>
-          </form>
-        </div>
-        <div>
-          <button onClick={handleClear}>Remove all items</button>
+        <form onSubmit={handleSubmit}>
+          <div className="input_container">
+            <input type="text" placeholder="Enter new technologies..." />
+            <button type="submit" className="add_button">
+              ADD
+            </button>
+          </div>
+        </form>
+        <div className="button_holder">
+          <button onClick={handleClear} className="clear_button">
+            Remove all items
+          </button>
         </div>
       </div>
-      <ul className="card-container">
-        {items.map((item) => (
-          <li
-            className="card"
-            key={item.id}
-            onClick={() => handleRemoveItem(item.id)}
-          >
-            {item.text}
-          </li>
-        ))}
-      </ul>
+      <ul className="card-container">{renderItems()}</ul>
     </div>
   );
 }
